f828aabb2faaebe3d6e4c15b5e60cec7223e259e
Noodledoodlemacaroodle <160121077+Noodledoodlemacaroodle@users.noreply.github.com>
2024-02-21 16:05:48 +0100

    Initial commit

---
+++ Brain.gd
@@ -0,0 +1,19 @@
+extends Node3D
+@onready var hitBox = $Area3D
+@onready var anim = $AnimatedSprite3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	anim.play("BrainIdle")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+func _on_area_3d_body_entered(body):
+	if body = "res://player.tscn":
+		
