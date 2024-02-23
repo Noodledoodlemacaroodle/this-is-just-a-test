88e118424f00aef895d8ddc65d132698fff47e8c
Noodledoodlemacaroodle <160121077+Noodledoodlemacaroodle@users.noreply.github.com>
2024-02-23 15:35:55 +0100

    Tower defense test

---
+++ Tower Defense/main.gd
@@ -0,0 +1,16 @@
+extends Node2D
+@onready var enemy = preload("res://Soldier.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$SpawnTimer.start()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_spawn_timer_timeout():
+	var instance = enemy.instantiate()
+	instance.position = $Path2D.position
+	$Path2D.add_child(instance)
+	
